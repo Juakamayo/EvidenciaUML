e4609c8bcdebcf477cbd5cb815f17c419f4ea753
Juakamayo <j.pizarro02@ufromail.cl>
2025-10-17 13:36:13 -0300

    init

---
+++ src/Carritocompra.java
@@ -0,0 +1,30 @@
+public class Carritocompra {
+
+	private int productos;
+
+	public void CarritoCompra() {
+		// TODO - implement Carritocompra.CarritoCompra
+		throw new UnsupportedOperationException();
+	}
+
+	private int calcularTotal() {
+		// TODO - implement Carritocompra.calcularTotal
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param cant
+	 * @param precio
+	 */
+	private int subTotal(int cant, int precio) {
+		// TODO - implement Carritocompra.subTotal
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarTotal() {
+		// TODO - implement Carritocompra.mostrarTotal
+		throw new UnsupportedOperationException();
+	}
+
+}
